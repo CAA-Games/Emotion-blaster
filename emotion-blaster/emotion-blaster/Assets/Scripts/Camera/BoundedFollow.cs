ff23df3a13728a7b05ec58b5595606af93c1d452
41407 <jimi.welling@cs.helsinki.fi>
2015-03-29 17:26:09 +0300

    Add camera script

---
+++ emotion-blaster/emotion-blaster/Assets/Scripts/Camera/BoundedFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundedFollow : MonoBehaviour
+{
+	public GameObject player;
+	public float boundedness;
+	public float z = -10;
+
+	void Update ()
+	{
+		Vector3 position = player.transform.position / boundedness;
+		position.z = z;
+		transform.position = position;
+	}
+}
